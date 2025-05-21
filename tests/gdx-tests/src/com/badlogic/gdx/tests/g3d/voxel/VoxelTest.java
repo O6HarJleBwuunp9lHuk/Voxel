b8ddd21cb68f7932ee7ef1ad3dc3208a5ec10516
O6HarJleBwuunp9lHuk <anri23092003@gmail.com>
2025-05-22 00:10:43 +0300

    voxel fiz

--- tests/gdx-tests/src/com/badlogic/gdx/tests/g3d/voxel/VoxelTest.java
+++ tests/gdx-tests/src/com/badlogic/gdx/tests/g3d/voxel/VoxelTest.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2011 See AUTHORS file.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,9 +16,8 @@
 
 package com.badlogic.gdx.tests.g3d.voxel;
 
-import com.badlogic.gdx.ApplicationListener;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.*;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
@@ -27,12 +26,26 @@ import com.badlogic.gdx.graphics.g3d.Environment;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.shaders.DefaultShader;
 import com.badlogic.gdx.graphics.g3d.utils.FirstPersonCameraController;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.math.collision.Ray;
+import com.badlogic.gdx.physics.bullet.collision.btCollisionObject;
+import com.badlogic.gdx.physics.bullet.collision.btCollisionObjectArray;
+import com.badlogic.gdx.physics.bullet.collision.btCollisionShape;
+import com.badlogic.gdx.physics.bullet.collision.btSphereShape;
+import com.badlogic.gdx.physics.bullet.dynamics.btRigidBody;
+import com.badlogic.gdx.physics.bullet.linearmath.btDefaultMotionState;
+import com.badlogic.gdx.physics.bullet.linearmath.btMotionState;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 import java.io.IOException;
 
 import static com.badlogic.gdx.tests.g3d.voxel.VoxelLoader.loadFromVoxFile;
+import static com.badlogic.gdx.tests.g3d.voxel.VoxelWorld.*;
 
 
 public class VoxelTest extends InputAdapter implements ApplicationListener {
@@ -43,7 +56,7 @@ public class VoxelTest extends InputAdapter implements ApplicationListener {
 	Environment lights;
 	FirstPersonCameraController controller;
 	VoxelWorld voxelWorld;
-
+    private Array<VoxelPhysicsBody> activePhysicsBodies = new Array<>();
 
 	public void create () {
 		//spriteBatch = new SpriteBatch();
@@ -54,7 +67,7 @@ public class VoxelTest extends InputAdapter implements ApplicationListener {
 		camera.near = 0.5f;
 		camera.far = 1000;
 		controller = new FirstPersonCameraController(camera);
-		Gdx.input.setInputProcessor(controller);
+        //Gdx.input.setInputProcessor(controller);
 
 		MathUtils.random.setSeed(0);
         try {
@@ -69,22 +82,94 @@ public class VoxelTest extends InputAdapter implements ApplicationListener {
 		camera.position.set(8f, 5f, 15f);
 		camera.lookAt(80,32,80);
 
-
+        Gdx.input.setInputProcessor(new InputMultiplexer(controller, new InputAdapter() {
+//            @Override
+//            public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+//                if (button == Input.Buttons.LEFT) {
+//                    // Получаем луч из камеры в точку клика
+//                    Ray ray = Raycaster.getPickRay(camera, screenX, screenY);
+//                    System.out.println("buubg");
+//                    // Проверяем пересечение с вокселями (простой вариант)
+//                    for (float t = 0; t < 20; t += 0.5f) {
+//                        Vector3 point = new Vector3(ray.origin).mulAdd(ray.direction, t);
+//                        int x = (int)point.x;
+//                        int y = (int)point.y;
+//                        int z = (int)point.z;
+//
+//                        if (voxelWorld.get(x, y, z) > 0) {
+//                            voxelWorld.breakVoxel(x, y, z, point, 10f);
+//                            return true;
+//                        }
+//                    }
+//                }
+//                return false;
+//            }
+@Override
+public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+    if (button == Input.Buttons.LEFT) {
+        throwObject();
+        return true;
+    }
+    return false;
+}
+        }));
 
 	}
 
+    private void throwObject() {
+        Vector3 direction = camera.direction.cpy().nor().scl(100);
+        Vector3 position = camera.position.cpy().add(camera.direction.cpy().nor().scl(2));
+
+        btCollisionShape shape = new btSphereShape(0.5f);
+        btMotionState motionState = new btDefaultMotionState();
+        btRigidBody.btRigidBodyConstructionInfo constructionInfo =
+            new btRigidBody.btRigidBodyConstructionInfo(1f, motionState, shape);
+
+        btRigidBody projectile = new btRigidBody(constructionInfo);
+        projectile.setWorldTransform(new Matrix4().setToTranslation(position));
+        projectile.setLinearVelocity(direction);
+        projectile.setUserValue(PROJECTILE_MARKER);
+
+        voxelWorld.addBody(projectile, PROJECTILE_GROUP, VOXEL_MASK);
+    }
 
 	public void render () {
-		ScreenUtils.clear(0.4f, 0.4f, 0.4f, 1f, true);
+        ScreenUtils.clear(0.4f, 0.4f, 0.4f, 1f, true);
+        float delta = Gdx.graphics.getDeltaTime();
+        voxelWorld.dynamicsWorld.stepSimulation(delta);
 		modelBatch.begin(camera);
 		modelBatch.render(voxelWorld, lights);
 		modelBatch.end();
 		controller.update();
+        voxelWorld.update(Gdx.graphics.getDeltaTime());
+        ShapeRenderer shapeRenderer = new ShapeRenderer();
+        shapeRenderer.setProjectionMatrix(camera.combined);
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
 
+
+        btCollisionObjectArray objects = voxelWorld.dynamicsWorld.getCollisionObjectArray();
+        System.out.println("Total physics objects: " + objects.size());
+
+        for (int i = 0; i < objects.size(); i++) {
+            btCollisionObject obj = objects.atConst(i);
+
+            Matrix4 transform = new Matrix4();
+            obj.getWorldTransform(transform);
+            Vector3 pos = transform.getTranslation(new Vector3());
+
+            System.out.println("Object " + i + " at: " + pos);
+
+            shapeRenderer.setColor(Color.RED);
+            shapeRenderer.box(pos.x - 0.5f, pos.y - 0.5f, pos.z - 0.5f, 1, 1, 1);
+        }
+
+        shapeRenderer.end();
 		//spriteBatch.begin();
 		//font.draw(spriteBatch, "fps: " + Gdx.graphics.getFramesPerSecond(), 0, 20);
 		//spriteBatch.end();
 	}
+
+
 
 	@Override
 	public void pause() {
--- tests/gdx-tests/src/com/badlogic/gdx/tests/g3d/voxel/VoxelWorld.java
+++ tests/gdx-tests/src/com/badlogic/gdx/tests/g3d/voxel/VoxelWorld.java
@@ -8,9 +8,14 @@ import com.badlogic.gdx.graphics.VertexAttribute;
 import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.Renderable;
 import com.badlogic.gdx.graphics.g3d.RenderableProvider;
+import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Pool;
+import com.badlogic.gdx.physics.bullet.Bullet;
+import com.badlogic.gdx.physics.bullet.collision.*;
+import com.badlogic.gdx.physics.bullet.dynamics.*;
+import com.badlogic.gdx.utils.Disposable;
 
 public class VoxelWorld implements RenderableProvider {
 	public static final int CHUNK_SIZE_X = 16;
@@ -31,12 +36,35 @@ public class VoxelWorld implements RenderableProvider {
 	public final int voxelsY;
 	public final int voxelsZ;
 
+    public static final int PROJECTILE_MARKER = 1;  // Маркер для снарядов
+    public static final int VOXEL_MARKER = 2;      // Маркер для вокселей
+    public static final int GROUND_MARKER = 3;     // Маркер для земли
+
+    // Группы коллизий (должны быть степенями 2)
+    public static final short GROUND_GROUP = 1 << 0;  // 1
+    public static final short VOXEL_GROUP = 1 << 1;   // 2
+    public static final short PROJECTILE_GROUP = 1 << 2; // 4
+
+
+    public static final short PROJECTILE_MASK = VOXEL_GROUP | GROUND_GROUP;
+    public static final short VOXEL_MASK = PROJECTILE_GROUP;
+
 	public int renderedChunks;
 	public int numChunks;
 
 	public final int[] palette = new int[256];
 	public boolean useColors = false;
 	private final int[][][] voxelColors;
+
+    private btCollisionConfiguration collisionConfig;
+    private btDispatcher dispatcher;
+    private btBroadphaseInterface broadphase;
+    private btConstraintSolver solver;
+    public btDynamicsWorld dynamicsWorld;
+
+
+
+    Array<btRigidBody> physicsBodies = new Array<>();
 
 	public VoxelWorld( int chunksX, int chunksY, int chunksZ) {
 		this.chunksX = chunksX;
@@ -53,6 +81,8 @@ public class VoxelWorld implements RenderableProvider {
 		this.numVertices = new int[numChunks];
 		this.voxelColors = new int[voxelsX][voxelsY][voxelsZ];
 
+
+
 		int index = 0;
 		for (int y = 0; y < chunksY; y++) {
 			for (int z = 0; z < chunksZ; z++) {
@@ -68,7 +98,55 @@ public class VoxelWorld implements RenderableProvider {
 		}
 
 		createMeshesAndMaterials();
+
+        Bullet.init();
+        collisionConfig = new btDefaultCollisionConfiguration();
+        dispatcher = new btCollisionDispatcher(collisionConfig);
+        broadphase = new btDbvtBroadphase();
+        solver = new btSequentialImpulseConstraintSolver();
+        dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, broadphase, solver, collisionConfig);
+        dynamicsWorld.setGravity(new Vector3(0, -9.8f, 0));
+        //dynamicsWorld.addRigidBody(groundBody, STATIC_GROUP, PROJECTILE_MASK);
+        btBoxShape groundShape = new btBoxShape(new Vector3(100, 1, 100));
+        btRigidBody.btRigidBodyConstructionInfo groundInfo =
+            new btRigidBody.btRigidBodyConstructionInfo(0, null, groundShape);
+
+        btRigidBody ground = new btRigidBody(groundInfo);
+        ground.setWorldTransform(new Matrix4().setToTranslation(0, -5, 0));
+        ground.setUserValue(VoxelWorld.GROUND_MARKER);
+        ground.setUserPointer(System.nanoTime());// Маркер для земли
+
+        // Добавляем в мир
+        this.addBody(ground, GROUND_GROUP, PROJECTILE_MASK);
+
+
 	}
+
+    public void update(float deltaTime) {
+        dynamicsWorld.stepSimulation(deltaTime);
+    }
+
+    public void addPhysicsBody(btRigidBody body) {
+        dynamicsWorld.addRigidBody(body);
+        System.out.println("Bodies in world: " + dynamicsWorld.getNumCollisionObjects());
+        physicsBodies.add(body);
+
+    }
+
+    public void dispose() {
+        for (btRigidBody body : physicsBodies) {
+            dynamicsWorld.removeRigidBody(body);
+            body.dispose();
+        }
+        physicsBodies.clear();
+
+        dynamicsWorld.dispose();
+        solver.dispose();
+        broadphase.dispose();
+        dispatcher.dispose();
+        collisionConfig.dispose();
+    }
+
 
 	private void createMeshesAndMaterials() {
 		VertexAttribute[] attributes = new VertexAttribute[] {
@@ -109,6 +187,8 @@ public class VoxelWorld implements RenderableProvider {
 			return;
 		}
 		voxelColors[x][y][z] = color;
+
+
 
 		int chunkX = x / CHUNK_SIZE_X;
 		int chunkY = y / CHUNK_SIZE_Y;
@@ -158,6 +238,11 @@ public class VoxelWorld implements RenderableProvider {
 		return 0;
 	}
 
+    public void addBody(btRigidBody body, short group, short mask) {
+        dynamicsWorld.addRigidBody(body, group, mask);
+        physicsBodies.add(body);
+    }
+
 	@Override
 	public void getRenderables(Array<Renderable> renderables, Pool<Renderable> pool) {
 		renderedChunks = 0;
@@ -186,4 +271,15 @@ public class VoxelWorld implements RenderableProvider {
 			}
 		}
 	}
+
+    public void breakVoxel(int x, int y, int z, Vector3 impactPoint, float force) {
+        if (get(x, y, z) == 0) return;
+
+        System.out.println("Destroying voxel at: " + x + ", " + y + ", " + z);
+        setVoxelWithColor(x, y, z, (byte)0, 0); // Удаляем воксель
+
+        // Опционально: создаем эффект разрушения (частицы, звук)
+        // ...
+    }
+
 }
